932c631b2177f416247b2c6a8fbba238fa71b33b
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-07-28 11:07:28 +0200

    Update `cross` installation to specific git rev
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -5,7 +5,7 @@
 set -exuo pipefail
 
 # Install deps
-cross --version || cargo install cross
+cross --version || cargo install cross --git https://github.com/cross-rs/cross --rev e281947ca900da425e4ecea7483cfde646c8a1ea
 
 # Build
 cross build -p nostr-sdk-ffi --target x86_64-unknown-freebsd --release
