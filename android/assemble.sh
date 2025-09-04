6b6c14dedbcc55104e3d62da85f1c21bd4477043
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-09-04 09:05:23 +0200

    Generate Android Kotlin foreign languages from debug binary
    Follow-up of f49d091ce5decdd9bbb7986b59ff617374adec2a
    
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- android/assemble.sh
+++ android/assemble.sh
@@ -4,7 +4,6 @@ set -exuo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TARGET_DIR="${SCRIPT_DIR}/../target"
-UNIFFI_CONFIG_PATH="${SCRIPT_DIR}/../uniffi-android.toml"
 ANDROID_MAIN_DIR="${SCRIPT_DIR}/lib/src/main"
 ANDROID_MAIN_KOTLIN_DIR="${ANDROID_MAIN_DIR}/kotlin"
 ANDROID_MAIN_JNI_LIBS_DIR="${ANDROID_MAIN_DIR}/jniLibs"
@@ -23,9 +22,12 @@ cp "${TARGET_DIR}/armv7-linux-androideabi/release/libnostr_sdk_ffi.so" "${ANDROI
 cp "${TARGET_DIR}/i686-linux-android/release/libnostr_sdk_ffi.so" "${ANDROID_MAIN_JNI_LIBS_DIR}/x86/"
 cp "${TARGET_DIR}/x86_64-linux-android/release/libnostr_sdk_ffi.so" "${ANDROID_MAIN_JNI_LIBS_DIR}/x86_64"
 
-# Generate Kotlin bindings
+# Debug
+ls -l "${TARGET_DIR}/uniffi/android-kotlin/"
+
+# Copy Kotlin bindings
 mkdir -p "${ANDROID_MAIN_KOTLIN_DIR}"
-cargo run --bin uniffi-bindgen generate --library "${TARGET_DIR}/aarch64-linux-android/release/libnostr_sdk_ffi.so" --config "${UNIFFI_CONFIG_PATH}" --language kotlin --no-format -o "${ANDROID_MAIN_KOTLIN_DIR}"
+cp -R "${TARGET_DIR}/uniffi/android-kotlin/." "${ANDROID_MAIN_KOTLIN_DIR}"
 
 # Assemble AAR
 "${SCRIPT_DIR}/gradlew" assembleRelease
